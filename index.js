315c2ea5d9359a6276c7602e324505609d0c703c
JaneSu <sujian@vikadata.com>
2021-07-21 11:40:58 +0800

    chore: 更新配置

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 module.exports = {
-
-  globals: {
-    MyGlobal: true
-  },
-
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+    "react-hooks",
+    "react"
+  ],
   rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
